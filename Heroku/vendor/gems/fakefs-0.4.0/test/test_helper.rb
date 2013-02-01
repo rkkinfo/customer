0e5445da99145361918b9e621a1e45b40907f119
dtutrainee <shubhamsharma160792@gmail.com>
2013-02-01 19:24:15 +0530

    Remove heroku

--- Heroku/vendor/gems/fakefs-0.4.0/test/test_helper.rb
+++
@@ -1,8 +0,0 @@
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
-require 'fakefs/safe'
-require 'test/unit'
-
-begin
-  require 'redgreen'
-rescue LoadError
-end
--- Heroku/vendor/gems/webmock-1.8.3/lib/webmock/version.rb
+++
@@ -1,3 +0,0 @@
-module WebMock
-  VERSION = '1.8.3' unless defined?(::WebMock::VERSION)
-end
